3cbda9a9c7a7821f8fdb101ecc6207b3f9933ed7
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-11-07 13:53:35 -0800

    Add absolute paths

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: `./404.html`
+			fallback: `404.html`
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			relative: false,
 		}
 	}
 };
