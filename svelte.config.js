ff924beb4bab5e5ca76d0952c2e98ceeb426c9f6
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-11-07 13:46:30 -0800

    now?

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 // svelte.config.js
 import adapter from '@sveltejs/adapter-static';
-import base from '$app/paths';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: `${base}/404.html`
+			fallback: `${process.env.BASE_PATH}/404.html`
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
